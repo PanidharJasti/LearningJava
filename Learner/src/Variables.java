6d27d88a5718d057b4377f4422b39553a3b4572b
Sunny <learnerjava15@gmail.com>
2025-02-16 22:27:02 -0500

    LearningJava-0001 Executing Variables

---
+++ Learner/src/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+
+    public  static void main (String[] args){
+        int a =2;
+        double b = 28.5;
+        char grade ='A';
+        boolean isJava = true;
+        String name = "Panidhar";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(grade);
+        System.out.println(isJava);
+        System.out.println(name);
+
+
+    }
+}
